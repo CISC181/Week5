916e3050ef01251e52ef6d291c3a2803c9576e66
Bert.Gibbons <bgibbons@udel.edu>
2014-09-18 14:04:41 -0400

    added new package

---
+++ CISC181.Week5/src/CISC181/Week5/Package4/cMain.java
@@ -0,0 +1,20 @@
+package CISC181.Week5.Package4;
+
+public class cMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static boolean ReturnTrue()
+	{
+		return true;		
+	}
+	
+	public static int AddTwoNumbers(int nbr1, int nbr2)
+	{
+		return nbr1 + nbr2;		
+	}
+
+}

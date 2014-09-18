498318d36ee798cad7c618128f686dfc6b7c08a9
Bert.Gibbons <bgibbons@udel.edu>
2014-09-18 13:20:03 -0400

    week 5 update

--- CISC181.Week5/src/CISC181/Week5/Package3/CMain.java
+++ CISC181.Week5/src/CISC181/Week5/Package3/CMain.java
@@ -3,7 +3,7 @@ package CISC181.Week5.Package3;
 public class CMain {
 	public static void main(String[] args) {
 		double mass;
-		mass = 175;
+		mass = 200;
 		for (Planet p : Planet.values()) {
 			System.out.printf("Your weight on %s is %f%n", p,
 					p.surfaceWeight(mass));
